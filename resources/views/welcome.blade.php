612629854e304f1b4970099598ff81f2a5a92e44
Dakimokurovich <ivan.t.vanya60@gmail.com>
2022-12-04 19:01:24 +0200

    infyomlabs/laravel-generator and artisan

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+
+        <style>
+            body {
+                font-family: 'Nunito', sans-serif;
+                font-size: 5rem;
+                display: flex;
+                height: 100vh;
+                justify-content: center;
+                align-items: center;
+            }
+        </style>
+    </head>
+    <body>
+       
+        <h1>Ткаченко</h1>
+    
+    </body>
+</html>
